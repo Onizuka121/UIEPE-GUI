47a3ad03600cadde2c32a1f1c5c6c7c0bc28c619
Onizuka121 <134316745+Onizuka121@users.noreply.github.com>
2023-12-26 20:39:04 +0100

    Update inserisci pratica pagina

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-
 var text = false;
 
-function ShowPass(){
+function ShowPass() {
+  var input = document.getElementById("pass");
+  var div = document.getElementById("view_id");
 
-    var input = document.getElementById("pass");
-    var div = document.getElementById("view_id");
-
-    if(!text){
-        input.type = 'text';
-        div.style.backgroundImage = "url('hide.png')";
-        text = true;
-    }else{
-        input.type = "password";
-        div.style.backgroundImage = "url('eye.png')";
-        text = false;
-    }
-
+  if (!text) {
+    input.type = "text";
+    div.style.backgroundImage = "url('hide.png')";
+    text = true;
+  } else {
+    input.type = "password";
+    div.style.backgroundImage = "url('eye.png')";
+    text = false;
+  }
 }
 
+function ShowAltro(par) {
+  if (par == "ist") {
+    let div = document.getElementById("altro_div_ist");
+    if (document.getElementById("ist_detenzione").value == "altro istituto") {
+      div.innerHTML =
+        ' <input type="text" name="altro_ist_detenzione" class="input-in-pratica" placeholder="Inserisci istituto di detenzione" style="width:70%;">';
+      return;
+    }
+    div.innerHTML = "";
+  }else{
+    let div = document.getElementById("altro_div");
+    if (document.getElementById("pos_giuridica").value == "altro") {
+      div.innerHTML =
+        ' <input type="text" name="altro_posizione_giuridica" class="input-in-pratica" placeholder="Inserisci posizione giuridica" style="width:70%;">';
+      return;
+    }
+    div.innerHTML = "";
+  }
 
-
+}
